776eb71c820cfbb053a8700cf1eac8a77fa53163
username <wnt925@gmail.com>
2019-05-31 19:53:13 -0600

    Create chars to ord value method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,4 +1,17 @@
 class Encrypt
-  def initiailze(message, key, date)
+
+  def initialize(message)
+    @message = message
+  end
+
+  def message_to_ord_values
+    chars = @message.downcase.chars
+    chars.map {|char| char.ord}
+  end
+
+  def random_number_generator
+  end
+
+  def create_offset_keys
   end
 end
